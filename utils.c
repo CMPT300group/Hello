8eb22774dc7863593f596c0d21ca5fc5568edef1
TestUserCmpt <91925063+TestUserCmpt@users.noreply.github.com>
2021-10-07 13:12:58 -0700

    Update utils.c

--- utils.c
+++ utils.c
@@ -1,66 +1,109 @@
 #include "utils.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <inttypes.h>
 
-
-//sign extends a bitfield with given size
-/* You may find implementing this function helpful */
-int bitSigner( unsigned int field, unsigned int size){
-    /* YOUR CODE HERE */
-    unsigned int ret;
-    unsigned mask = 0xffffffff << size;
-    if ((field & (1 << (size - 1))) != 0){
-        // number is negative
-        ret = field | mask;
-    }
-    else {
-        // number is positive
-        ret = field & ~mask;
-    }
-    return (int) ret;
+/* From stack overflow post number 10090326 */
+int extract(int value, int begin, int end)
+{
+    int mask = (1 << (end - begin)) - 1;
+    return (value >> begin) & mask;
 }
 
-/* Remember that the offsets should return the offset in BYTES */
+/* Sign extends the given field to a 32-bit integer where field is
+ * interpreted an n-bit integer. */ 
+int sign_extend_number( unsigned int field, unsigned int n) {
+    int a;
+    if (extract(field, n-1, n) == 0) {
+       a = field;
+    } else {
+    int value = 0;
+    for (int i = 0; i < 32-n; i++) {
+        value = value*2 + 1;
+    }
+    a = field + (value << n);
+    }
+    return a;
+}
 
+/* Unpacks the 32-bit machine code instruction given into the correct
+ * type within the instruction struct */ 
+Instruction parse_instruction(uint32_t instruction_bits) {
+    /* YOUR CODE HERE */
+    Instruction instruction;
+
+    /* Opcode is always first 7 bits. */
+    instruction.opcode = extract(instruction_bits, 0, 7);
+
+    /* Make rest moniker to have way to obtain rest of bits. */
+    instruction.rest = extract(instruction_bits, 7, 32);
+
+    /* Do splitting assuming it is r type. */
+    instruction.rtype.rd = extract(instruction_bits, 7, 12);
+    instruction.rtype.funct3 = extract(instruction_bits, 12, 15);
+    instruction.rtype.rs1 = extract(instruction_bits, 15, 20);
+    instruction.rtype.rs2 = extract(instruction_bits, 20, 25);
+    instruction.rtype.funct7 = extract(instruction_bits, 25, 32);
+
+    
+    /* Do splitting assuming it is i type. */
+    instruction.itype.rd = extract(instruction_bits, 7, 12);
+    instruction.itype.funct3 = extract(instruction_bits, 12, 15);
+    instruction.itype.rs1 = extract(instruction_bits, 15, 20);
+    instruction.itype.imm = extract(instruction_bits, 20, 32);
+
+
+    /* Do splitting assuming it is u type. */
+    instruction.utype.rd = extract(instruction_bits, 7, 12);
+    instruction.utype.imm = extract(instruction_bits, 12, 32);
+
+    /* Do splitting assuming it is uj type. */
+    instruction.ujtype.rd = extract(instruction_bits, 7, 12);
+    instruction.ujtype.imm = extract(instruction_bits, 12, 32);
+
+    /* Do splitting assuming it is s type. */
+    instruction.stype.imm5 = extract(instruction_bits, 7, 12);
+    instruction.stype.funct3 = extract(instruction_bits, 12, 15);
+    instruction.stype.rs1 = extract(instruction_bits, 15, 20);
+    instruction.stype.rs2 = extract(instruction_bits, 20, 25);
+    instruction.stype.imm7 = extract(instruction_bits, 25, 32);
+
+    /* Do splitting assuming it is sb type. */
+    instruction.sbtype.imm5 = extract(instruction_bits, 7, 12);
+    instruction.sbtype.funct3 = extract(instruction_bits, 12, 15);
+    instruction.sbtype.rs1 = extract(instruction_bits, 15, 20);
+    instruction.sbtype.rs2 = extract(instruction_bits, 20, 25);
+    instruction.sbtype.imm7 = extract(instruction_bits, 25, 32);
+
+    return instruction;
+}
+
+/* Return the number of bytes (from the current PC) to the branch label using the given
+ * branch instruction */
 int get_branch_offset(Instruction instruction) {
-    /* YOUR CODE HERE */
-    unsigned int mask1 = (1 << 4) - 1;
-    unsigned int mask2  = (1 << 6) - 1;
-    unsigned int first4bits = (instruction.sbtype.imm5 >> 1) & mask1;
-    unsigned int bits4to9 = (instruction.sbtype.imm7) & mask2;
-    unsigned int bit10 = instruction.sbtype.imm5 & 1;
-    unsigned int bit11 = instruction.sbtype.imm7 >> 6 & 1;
-    unsigned int offset = (bit11 << 1) | bit10;
-    offset = (offset << 6) | bits4to9;
-    offset = (offset << 4) | first4bits;
-    offset = offset << 1;
-    return bitSigner(offset, 12);
+    
+    int imm1 = extract(instruction.sbtype.imm5, 0, 1);
+    int imm2 = extract(instruction.sbtype.imm5, 1, 5);
+    int imm3 = extract(instruction.sbtype.imm7, 0, 6);
+    int imm4 = extract(instruction.sbtype.imm7, 6, 7);
+    int imm = (imm2 + 16*imm3 + 1024*imm1 + 2048*imm4)*2;
+    return sign_extend_number(imm, 13); 
 }
 
+/* Returns the number of bytes (from the current PC) to the jump label using the given
+ * jump instruction */
 int get_jump_offset(Instruction instruction) {
-    /* YOUR CODE HERE */
-    unsigned int mask1 = (1 << 10) - 1;
-    unsigned int mask2 = (1 << 8) - 1;
-    unsigned int first10bits = (instruction.ujtype.imm >> 9) & mask1;
-    unsigned int next8bits = (instruction.ujtype.imm) & mask2;
-    unsigned int bit11 = (instruction.ujtype.imm >> 8) & 1;
-    unsigned int bit20 = (instruction.ujtype.imm >> 19) & 1;
-    unsigned int offset = (bit20 << 8) | next8bits;
-    offset = (offset << 1) | bit11;
-    offset = (offset << 10) | first10bits;
-    offset = offset << 1;
-    return bitSigner(offset, 20);
-
-    // unsigned short extract(unsigned short value, int begin, int end)
-    // unsigned short mask = (1 << (end - begin)) - 1;
-    // return (value >> begin) & mask;
+    int imm1 = extract(instruction.ujtype.imm, 0, 8);
+    int imm2 = extract(instruction.ujtype.imm, 8, 9);
+    int imm3 = extract(instruction.ujtype.imm, 9, 19);
+    int imm4 = extract(instruction.ujtype.imm, 19, 20);
+    int imm = (imm3 + 1024*imm2 + 2048*imm1 + 524288*imm4);
+    return sign_extend_number(imm<<1, 21);
 }
-
 
 int get_store_offset(Instruction instruction) {
-    /* YOUR CODE HERE */
-    unsigned int offset = (instruction.stype.imm7 << 5) | instruction.stype.imm5;
-    return bitSigner(offset, 12);
+    int imm = instruction.stype.imm5 + 32*instruction.stype.imm7;
+    return sign_extend_number(imm, 12);
 }
 
 void handle_invalid_instruction(Instruction instruction) {
