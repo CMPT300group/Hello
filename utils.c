ed706c00efabeba8759eb411a6e58bc42613debc
TestUserCmpt <91925063+TestUserCmpt@users.noreply.github.com>
2021-10-07 13:05:11 -0700

    Update utils.c

--- utils.c
+++ utils.c
@@ -2,64 +2,69 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/* Sign extends the given field to a 32-bit integer where field is
- * interpreted an n-bit integer. Look in test_utils.c for examples. */ 
-int sign_extend_number( unsigned int field, unsigned int n) {
-    unsigned bitStatus = (field >> (n-1) ) & 1; 
-    if(!bitStatus) return field;
-    for(int i = n; i < 32; i++) {
-        field = ((bitStatus << i)| (field & (~(1 << i))));
+
+//sign extends a bitfield with given size
+/* You may find implementing this function helpful */
+int bitSigner( unsigned int field, unsigned int size){
+    /* YOUR CODE HERE */
+    unsigned int ret;
+    unsigned mask = 0xffffffff << size;
+    if ((field & (1 << (size - 1))) != 0){
+        // number is negative
+        ret = field | mask;
     }
-    return field;
+    else {
+        // number is positive
+        ret = field & ~mask;
+    }
+    return (int) ret;
 }
 
-/* Unpacks the 32-bit machine code instruction given into the correct
- * type within the instruction struct. Look at types.h */ 
-Instruction parse_instruction(uint32_t instruction_bits) {
-    Instruction instruction;
-    instruction.bits = instruction_bits;
-    return instruction;
-}
+/* Remember that the offsets should return the offset in BYTES */
 
-/* Return the number of bytes (from the current PC) to the branch label using the given
- * branch instruction */
 int get_branch_offset(Instruction instruction) {
-    unsigned offset = 0;
-    int imm = instruction.sbtype.imm5;
-    offset = (0x1e & imm)|offset;
-    offset = (0x800 & (imm<< 11))|offset;
-    imm = instruction.sbtype.imm7;
-    offset = (0x7e0 & (imm << 5))|offset;
-    offset = (0x1000 & (imm << 6))|offset;
-    return sign_extend_number(offset, 13);
+    /* YOUR CODE HERE */
+    unsigned int mask1 = (1 << 4) - 1;
+    unsigned int mask2  = (1 << 6) - 1;
+    unsigned int first4bits = (instruction.sbtype.imm5 >> 1) & mask1;
+    unsigned int bits4to9 = (instruction.sbtype.imm7) & mask2;
+    unsigned int bit10 = instruction.sbtype.imm5 & 1;
+    unsigned int bit11 = instruction.sbtype.imm7 >> 6 & 1;
+    unsigned int offset = (bit11 << 1) | bit10;
+    offset = (offset << 6) | bits4to9;
+    offset = (offset << 4) | first4bits;
+    offset = offset << 1;
+    return bitSigner(offset, 12);
 }
 
-/* Returns the number of bytes (from the current PC) to the jump label using the given
- * jump instruction */
 int get_jump_offset(Instruction instruction) {
-    unsigned offset = 0;
-    int imm = instruction.ujtype.imm;
-    offset = (0x800 & (imm << 3))|offset;
-    offset = (0x7fe & (imm >> 8))|offset;
-    offset = (0xff000 & (imm << 12))|offset;
-    offset = (0x100000 & (imm << 2))|offset;
-    return sign_extend_number(offset, 21);
+    /* YOUR CODE HERE */
+    unsigned int mask1 = (1 << 10) - 1;
+    unsigned int mask2 = (1 << 8) - 1;
+    unsigned int first10bits = (instruction.ujtype.imm >> 9) & mask1;
+    unsigned int next8bits = (instruction.ujtype.imm) & mask2;
+    unsigned int bit11 = (instruction.ujtype.imm >> 8) & 1;
+    unsigned int bit20 = (instruction.ujtype.imm >> 19) & 1;
+    unsigned int offset = (bit20 << 8) | next8bits;
+    offset = (offset << 1) | bit11;
+    offset = (offset << 10) | first10bits;
+    offset = offset << 1;
+    return bitSigner(offset, 20);
+
+    // unsigned short extract(unsigned short value, int begin, int end)
+    // unsigned short mask = (1 << (end - begin)) - 1;
+    // return (value >> begin) & mask;
 }
 
-/* Returns the byte offset (from the address in rs2) for storing info using the given
- * store instruction */ 
+
 int get_store_offset(Instruction instruction) {
-    unsigned offset = 0;
-    int imm = instruction.stype.imm5;
-    offset = (0x1f & imm)|offset;
-    imm = instruction.stype.imm7;
-    offset = (0xfe0 & (imm << 5))|offset;
-    return sign_extend_number(offset, 12);
+    /* YOUR CODE HERE */
+    unsigned int offset = (instruction.stype.imm7 << 5) | instruction.stype.imm5;
+    return bitSigner(offset, 12);
 }
 
 void handle_invalid_instruction(Instruction instruction) {
     printf("Invalid Instruction: 0x%08x\n", instruction.bits); 
-    exit(-1);
 }
 
 void handle_invalid_read(Address address) {
